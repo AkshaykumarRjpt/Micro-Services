d64eaf916093b61d8dd8295a2ada97a451128c44
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-08-23 22:12:01 +0530

    Added Basic UI - (MVC)
    Layout.cs
    viewstart
    viewImports
    bootstrap
    images

---
+++ GloboTicket/GloboTicket.WebClient/Controllers/EventsCatalogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GloboTicket.WebClient.Models.View;
+namespace GloboTicket.WebClient.Controllers
+{
+    public class EventsCatalogController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new EventListModel { });
+        }
+
+    }
+}
